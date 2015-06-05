3833b001194df7f705869a2153b4d828d4d1a968
Ben Holland <benholland99@gmail.com>
2015-06-05 18:09:47 +0100

    Initial dashboard firebase integration

--- app/js/asset.js
+++ app/js/asset.js
@@ -1,40 +1,55 @@
-require('firebase');
-var React = require('react');
-var ReactFireMixin = require('reactfire');
-
-console.log(ReactFireMixin);
+import Firebase from 'firebase';
+import React from 'react';
+import {unique} from 'lodash/array';
+import {find, map, reduce} from 'lodash/collection';
 
 var asset = React.createClass({
 
-    mixins: [ReactFireMixin],
-
     getInitialState: function() {
         return {
-            items: []
+            files: []
         };
     },
 
     componentWillMount: function() {
-        this.bindAsArray(new Firebase('https://asset-monitor.firebaseio.com/assets/css/'), 'items');
+        new Firebase('https://asset-monitor.firebaseio.com/assets/images').limitToLast(10).on('value', (data) => {
+            const builds = data.val();
+            const files = unique(reduce(map(builds, (build) => map(build, (asset) => asset.filename)), (arr, build) => arr.concat(build), []));
+
+            const assets = map(files, (file) => {
+                const history = map(builds, (build) => find(build, (asset) => asset.filename === file));
+
+                return {
+                    file: file,
+                    recent: history[history.length - 1],
+                    history: history
+                };
+            }).sort((a, b) => b.recent.uncompressed > a.recent.uncompressed);
+
+            this.setState({
+                files: assets
+            });
+        });
     },
 
     render: function() {
-        var blocks;
+        const historyStats = (asset) => {
+            return <h4>{(asset.uncompressed / 1024).toFixed(1)}kb</h4>;
+        };
 
-        if(!this.state.items[0]) {
-            blocks = <p>Loading...</p>;
-        } else {
-            blocks = this.state.items[0].map(function (block) {
-                return (
+        const blocks = this.state.files.map(function (asset, i) {
+            return (
+                <div className="col-md-4" key={'block' + i}>
                     <div className="block">
-                        <h1 className="block-title">{block.filename}</h1>
-                        <div className="block-data">{(block.uncompressed / 1024).toFixed(1)}kb</div>
+                        <h1 className="block-title">{asset.file}</h1>
+                        <div className="block-data">{(asset.recent.uncompressed / 1024).toFixed(1)}kb</div>
+                        <div className="block-title">{map(asset.history, historyStats)}</div>
                     </div>
-                );
-            });
-        }
+                </div>
+            );
+        });
 
-        return <div>{blocks}</div>;
+        return <div className="row">{blocks}</div>;
     }
 
 });
